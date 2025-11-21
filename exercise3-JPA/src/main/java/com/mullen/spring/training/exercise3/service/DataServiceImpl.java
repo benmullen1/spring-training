ec5f287706750e45c903838a30fd787ab7b03bf3
benmullen1@gmail.com <benmullen1@gmail.com>
2025-11-21 02:27:08 -0500

    Exercise 3 Initial Commit

--- exercise3-JPA/src/main/java/com/mullen/spring/training/exercise3/service/DataServiceImpl.java
+++ exercise3-JPA/src/main/java/com/mullen/spring/training/exercise3/service/DataServiceImpl.java
@@ -3,12 +3,8 @@ package com.mullen.spring.training.exercise3.service;
 import com.mullen.spring.training.exercise3.model.CourseJDBCRepository;
 import com.mullen.spring.training.exercise3.model.dto.Course;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
